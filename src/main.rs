c09ab6faa4e3534b57473d77a3679b6408a8fc6e
adadachanji@gmail.com <adadachanji@gmail.com>
2021-01-20 07:25:55 +0000

    Template working. Added some readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("adder: {:?}", utils::Adder::<i32>::add(3, 4));
+    println!("adder: {:?}", utils::Adder::<i32>::add(5, 6));
+
+    utils::test123();
+
+    utils::print_item(12);
+    utils::print_item("ertr");
+
+
+    println!("add int int:   {}", utils::Adder::<i32>::add(1, 2));
+    println!("add int float: {}", utils::Adder::<f64>::add(1, 2.3));
+
 }
